4cd862cd3803c3a5738b75e104d8e0e5798cb6fe
AndroidHQ254 <d.murimi14@gmail.com>
2023-01-19 18:17:55 +0300

    toco: Track LineageOS telephony

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,6 +3,7 @@ rm -rf vendor/xiaomi
 rm -rf hardware/xiaomi
 rm -rf kernel/xiaomi
 rm -rf prebuilts/clang/host/linux-x86/clang-playground
+rm -rf vendor/codeaurora/telephony
 
 git clone --depth=1 https://github.com/ProjectElixir-Devices/device_xiaomi_sm6150-common-toco.git -b tiramisu device/xiaomi/sm6150-common
 git clone --depth=1 https://github.com/AndroidHQ254/android_hardware_xiaomi.git -b 13 hardware/xiaomi
@@ -11,3 +12,4 @@ git clone --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git -b 16 pr
 git clone --depth=1 https://github.com/AndroidHQ254/vendor_xiaomi.git -b tiramisu vendor/xiaomi
 git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_google.git vendor/xiaomi/google
 git clone --depth=1 https://gitlab.com/AndroidHQ254/vendor-xiaomi-toco-miuicamera.git -b los vendor/xiaomi/toco-miuicamera
+git clone --depth=1 https://github.com/LineageOS/android_vendor_codeaurora_telephony.git -b lineage-20.0 vendor/codeaurora/telephony
